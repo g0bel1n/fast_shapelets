bb32ac35e7577b56426f4bd9de910719214b4775
Chirrane Sophia <sophia.chirrane@student-cs.fr>
2023-03-16 17:30:19 +0100

    vmap DTW

--- src/_utils.py
+++ src/_utils.py
@@ -43,6 +43,8 @@ def evaluate_gaussianness(X: np.ndarray, y=None) -> float:
 def DTW(s1,s2):
     return  dtw.distance_fast(np.array(s1,dtype=np.double), np.array(s2,dtype=np.double), use_pruning=True)
 
+DTW = jax.vmap(DTW, in_axes=(0, None))
+
 def get_splits(n_splits : int) -> np.ndarray:
     """
     It takes a column of data, calculates the mean and standard deviation, then creates a normal
