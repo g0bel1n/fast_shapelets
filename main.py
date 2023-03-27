586f7dd670a3e898ae0597d61bee856c7ce62acf
g0bel1n <lucas.saban@ensae.fr>
2023-03-27 17:57:20 +0000

    cleanup

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+
+def main(args):
+    X_train,y_train, X_test, y_test = get_dataset('StarLightCurves')
+    y_train = y_train-1
+    y_test = y_test-1
+        
+    shapelet_lengths = list(range(args.min_shap_len,args.max_shap_len+1, args.step_shap_len))
+    fs = FastShapelets(shapelet_lengths=shapelet_lengths, cardinality=args.cardinality, dimensionality=args.dimensionality, r=args.r, n_jobs=args.n_jobs, verbose=args.verbose)
+    fs.fit(X_train, y_train)
+    
+    fn_save = f'shap_{args.min_shap_len}_{args.max_shap_len+1}_{args.step_shap_len}.pkl'
+    with open(fn_save, 'wb') as f:
+        pickle.dump(fs.get_shapelets(), f)
+        
+    
+    print(f'file saved at {fn_save}')
+        
+    
 if __name__ == '__main__' :
     import sys 
     import os
@@ -18,7 +36,7 @@ if __name__ == '__main__' :
     parser.add_argument("--n_jobs", type=int, default=1, help="n jobs")
     parser.add_argument("--min_shap_len", type=int, default=100, help="min_shap_len")
     parser.add_argument("--max_shap_len", type=int, default=500, help="max_shap_len")
-    parser.add_argument("--step_shap_len", type=int, default=100, help="step_shap_len")
+    parser.add_argument("--step_shap_len", type=int, default=50, help="step_shap_len")
     parser.add_argument("--cardinality", type=int, default=4, help="cardinality")
     parser.add_argument("--dimensionality", type=int, default=16, help="dimensionality")
     parser.add_argument("--r", type=int, default=10, help="r")
@@ -29,19 +47,6 @@ if __name__ == '__main__' :
     
     args = parser.parse_args()
     
-    X_train,y_train, X_test, y_test = get_dataset('StarLightCurves')
-    y_train = y_train-1
-    y_test = y_test-1
-        
-    shapelet_lengths = list(range(args.min_shap_len,args.max_shap_len+1, args.step_shap_len))
-    fs = FastShapelets(shapelet_lengths=shapelet_lengths, cardinality=args.cardinality, dimensionality=args.dimensionality, r=args.r, n_jobs=args.n_jobs, verbose=args.verbose)
-    fs.fit(X_train, y_train)
-    
-    fn_save = f'shap_{args.min_shap_len}_{args.max_shap_len+1}_{args.step_shap_len}.pkl'
-    with open(fn_save, 'wb') as f:
-        pickle.dump(fs.get_shapelets(), f)
-        
-    
-    print(f'file saved at {fn_save}')
+    main(args)
         
     
